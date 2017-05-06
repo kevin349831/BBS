d3a45e61834598c3438c7cb0f0dea13c315394aa
kevin349831 <kevin349831@gmail.com>
2017-05-06 13:37:14 +0800

    Create Forum.java

---
+++ Forum.java
@@ -0,0 +1,18 @@
+
+
+package sockettest6;
+
+
+// Referenced classes of package sockettest6:
+//            Jdbc, Article
+
+public class Forum
+{
+
+    public Forum(int n)
+    {
+        Jdbc j = new Jdbc();
+    }
+
+    Article article[];
+}
